1a88ba50092f35ac3e0204472dae99005703422a
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-24 17:08:53 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -3,14 +3,21 @@
 #include "test/catch.hpp"
 #include "sensor-validate.h"
 
-TEST_CASE("reports error when soc jumps abruptly") 
+TEST_CASE("reports error when SOC jumps abruptly") 
 {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
   REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0); 
 }
 
-TEST_CASE("sensor is not checked for fault when a null input array is passed") 
+TEST_CASE("SOC sensor is valid when the input array doesnt record any abrupt spike") 
+{
+  double socReadings[] = {0.2, 0.21, 0.24, 0.29, 0.32, 0.35};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 1); 
+}
+
+TEST_CASE("SOC sensor is not checked for fault when a null input array is passed") 
 {
   double socReadings[] = {};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
@@ -22,6 +29,13 @@ TEST_CASE("reports error when current jumps abruptly")
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateCurrentreadings(currentReadings, numOfCurReadings) == 0);
+}
+
+TEST_CASE("Validation is positive when there are no abrupt spike in current ") 
+{
+  double currentReadings[] = {0.4, 0.45, 0.55, 0.65, 0.72, 0.75};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
+  REQUIRE(validateCurrentreadings(currentReadings, numOfCurReadings) == 1);
 }
 
 TEST_CASE("current sensor is not checked for fault when a null input array is passed") 
