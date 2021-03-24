ad069a114c5a513362ecacf94e2ed8d2fbb798ec
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-24 14:33:23 +0530

    Update sensor-validate.h
    Renamed the function to check the senor readings

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,5 +1,5 @@
 
-int _give_me_a_good_name(double value, double nextValue, double maxDelta);
+int SensorReadingIsFaulty(double value, double nextValue, double maxDelta);
 
 int validateSOCreadings(double* values, int numOfValues);
 
