9e8508e505560e17ac107759a3ff2c5e0bc71b3c
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-24 16:07:45 +0530

    Update test-validationtool.cpp
    added null check test case

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -3,14 +3,30 @@
 #include "test/catch.hpp"
 #include "sensor-validate.h"
 
-TEST_CASE("reports error when soc jumps abruptly") {
+TEST_CASE("reports error when soc jumps abruptly") 
+{
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0); 
 }
 
-TEST_CASE("reports error when current jumps abruptly") {
+TEST_CASE("sensor is not checked for fault when a null input array is passed") 
+{
+  double socReadings[] = {};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0); 
+}
+
+TEST_CASE("reports error when current jumps abruptly") 
+{
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
+  REQUIRE(validateCurrentreadings(currentReadings, numOfCurReadings) == 0);
+}
+
+TEST_CASE("sensor is not checked for fault when a null input array is passed") 
+{
+  double currentReadings[] = {};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateCurrentreadings(currentReadings, numOfCurReadings) == 0);
 }
