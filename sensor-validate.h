66b74218bb49c8e96931f060d243b7d59ea506e3
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-24 16:32:43 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,7 +1,18 @@
 
+/*Header file*/
+/* Indlude header files if any */
+/**********************************************
+
+***********************************************/
+/* Define Macros*/
+/**********************************************/
  #define DELTAMAXSOC 0.05
  #define DELTAMAXCURRENT 0.1
+/**********************************************/
 
+/****************************************************************************
+Function declaration
+***************************************************************************/
 bool inputArrayisNotEmpty(int numOfValues);
 
 bool SensorReadingIsFaulty(double value, double nextValue, double maxDelta);
